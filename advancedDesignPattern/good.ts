22e7b03fad705748970b10ba08770e7be0c08167
in-ch <sxin2949@naver.com>
2023-01-16 20:40:20 +0900

    refactor: class 분리

--- advancedDesignPattern/good.ts
+++ advancedDesignPattern/good.ts
@@ -1,47 +1,5 @@
 import { LOGINMODE, LoginType } from "./bad";
-
-interface Strategy {
-  authenticate(args: any[]): boolean;
-}
-
-class TwitterStrategy implements Strategy {
-  authenticate(args: any[]) {
-    const [token] = args;
-    if (token !== "tw123") {
-      console.error("Twitter account authentication failed!");
-      return false;
-    }
-    console.log("Twitter account authentication succeeded!");
-    return true;
-  }
-}
-
-class LocalStrategy implements Strategy {
-  authenticate(args: any[]) {
-    const [username, password] = args;
-    if (username !== "bytefer" || password !== "666") {
-      console.log("Incorrect username or password!");
-      return false;
-    }
-    console.log("Account and password authentication succeeded!");
-    return true;
-  }
-}
-
-class Authenticator {
-  strategies: Record<string, Strategy> = {};
-
-  use(name: string, strategy: Strategy) {
-    this.strategies[name] = strategy;
-  }
-  authenticate(name: string, ...args: any) {
-    if (!this.strategies[name]) {
-      console.error("Authentication policy has not been set!");
-      return false;
-    }
-    return this.strategies[name].authenticate.apply(null, args);
-  }
-}
+import { Authenticator, LocalStrategy, TwitterStrategy } from "./class";
 
 const auth = new Authenticator();
 
